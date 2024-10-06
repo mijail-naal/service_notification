3441d34c7b9d81190b82526d65f20ea0a57f4b56
Mijail N <89676694+mijail-naal@users.noreply.github.com>
2024-10-06 14:58:37 -0600

    Review task 5

--- worker/src/deliver/tasks.py
+++ worker/src/deliver/tasks.py
@@ -1,9 +1,13 @@
 import json
+import requests
+
+from uuid import UUID
 
 from pydantic import BaseModel
 
 from celery import shared_task
 from .noti_email import Notification
+from core.config import settings
 
 
 class TestNotification(BaseModel):
@@ -20,7 +24,7 @@ class TestNotification(BaseModel):
     the task 'send_notification'.
     """
 
-    id: int = '12345'
+    id: UUID = 'c3242d9b-4ff7-494e-9cc4-4fc9842c0ba1'
     username: str = 'John'
     email: str = 'test@mail.com'
     users: list = ['John']
@@ -32,9 +36,31 @@ class TestNotification(BaseModel):
 cn = TestNotification()
 
 
+def get_user_from_auth(user_id):
+    auth = requests.post(
+        settings.auth_signin_url,
+        json={'username': settings.auth_admin_name, 'password': settings.auth_password}
+    )
+    tokens = json.loads(auth.content)
+    res = requests.post(
+        settings.auth_get_user_url,
+        headers={
+            'Authorization': 'Bearer {access_token}'.format(
+                access_token=tokens['access_token']
+            )
+        },
+        json={'id': user_id}
+    )
+    return res.content.decode('utf-8')
+
+
 @shared_task
 def send_notification(body):
     decoded = body.decode('utf-8')
     data = json.loads(decoded)
+    user = get_user_from_auth(data['id'])
+    cn.id = user['id']
+    cn.username = user['first_name']
+    cn.email = user['email']
     notification = Notification()
     notification.send_email(cn)
